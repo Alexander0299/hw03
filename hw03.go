eecbad263c24aa1af31957f52fc73ee4255b7454
Alexander0299 <solovuev.alex@mail.ru>
2025-01-31 11:17:06 +0300

    Add files via upload

--- hw03.go
+++ hw03.go
@@ -1,1 +1,33 @@
+package main
 
+import (
+	"fmt"
+	"strings"
+)
+
+func chessboard(size int) string {
+	var board strings.Builder
+
+	for i := 0; i < size; i++ {
+		for j := 0; j < size; j++ {
+			switch (i + j) % 2 {
+			case 0:
+				board.WriteString(" ")
+			default:
+				board.WriteString("#")
+			}
+		}
+		board.WriteString("\n")
+	}
+
+	return board.String()
+}
+
+func main() {
+	var size int
+	fmt.Print("Введите размер шахматной доски: ")
+	fmt.Scanf("%d", &size)
+
+	cb := chessboard(size)
+	fmt.Println(cb)
+}
